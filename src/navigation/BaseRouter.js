cdec716cfc8269fa95653b62e09dbe4a93b8e9bf
hanmustofa <haniimustofa1@gmail.com>
2021-10-28 17:49:58 +0700

    update home
    memperbarui home

--- src/navigation/BaseRouter.js
+++ src/navigation/BaseRouter.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Saved from "../pages/Saved";
-import Indonesia from "../pages/Indonesia";
+import Home from "../pages/Indonesia";
 
 export default function BaseRouter() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route path="/indonesia" component={Indonesia} />
+      {/* <Route path="/indonesia" component={Indonesia} /> */}
       <Route path="/about" component={About} />
       <Route path="/saved" component={Saved} />
     </Switch>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
+// function Home() {
+//   return <h2>Home</h2>;
+// }
 
 function About() {
   return <h2>About</h2>;
