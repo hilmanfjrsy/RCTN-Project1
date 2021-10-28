d4a2d16bcd9682d430fd8d9f042777bcc1e7c6c3
hanmustofa <haniimustofa1@gmail.com>
2021-10-28 17:46:13 +0700

    pembaruan indonesia
    menambah pages indonesia

--- src/config/GlobalVar.js
+++ src/config/GlobalVar.js
@@ -1,5 +1,5 @@
 export const GlobalVar = {
-  baseColor:'#16425B',
-  greyColor:'#D9DCD6',
-  apiKey:'API_KEY'
-}
+  baseColor: "#16425B",
+  greyColor: "#D9DCD6",
+  apiKey: "525d071aed7f48798746d2ac7f01ae13",
+};
--- src/navigation/BaseRouter.js
+++ src/navigation/BaseRouter.js
@@ -1,16 +1,15 @@
 import React from "react";
-import {
-  Switch,
-  Route,
-} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Saved from "../pages/Saved";
+import Indonesia from "../pages/Indonesia";
 
 export default function BaseRouter() {
   return (
     <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/saved" component={Saved}/>
+      <Route exact path="/" component={Home} />
+      <Route path="/indonesia" component={Indonesia} />
+      <Route path="/about" component={About} />
+      <Route path="/saved" component={Saved} />
     </Switch>
   );
 }
