89a13f4434e893b9024046bf57cf621e5df5ebca
Hilman Fajri Fahriansyah <hilmanfajri24@gmail.com>
2021-10-29 07:42:09 +0700

    Update GlobalFunc.js

--- src/config/GlobalFunc.js
+++ src/config/GlobalFunc.js
@@ -5,7 +5,9 @@ import { GlobalVar } from "./GlobalVar";
 export async function getRequest(path) {
   try {
     const response = await axios.get('https://newsapi.org/' + path + "&apiKey=" + GlobalVar.apiKey);
-    return response;
+    if(response){
+      return response;
+    }
   } catch (error) {
     toast.error(error.message, {
       theme: "colored"
